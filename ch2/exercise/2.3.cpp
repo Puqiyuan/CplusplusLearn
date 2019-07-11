b9eb322e02d3f11f9c267bcfffe751069c3afa09
Qiyuan Pu <pqy7172@gmail.com>
2019-07-11 21:12:17 +0800

    upate for 2.3

---
+++ ch2/exercise/2.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << "\n";
+	std::cout << u - u2 << "\n";
+
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << "\n";
+	std::cout << i - i2 << "\n";
+
+	std::cout << i - u << "\n";
+	std::cout << u - i << "\n";
+	
+	return 0;
+}
